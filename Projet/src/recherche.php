d09613b7a290175bdab9d731ab37e26612b6a8e0
Mathis <mathis.embit6@etu.univ-lorraine.fr>
2021-12-14 18:46:43 +0100

    barre de recherche presque finie

--- Projet/src/recherche.php
+++ Projet/src/recherche.php
@@ -1,15 +1,19 @@
 <?php
-   // Vérifier si le formulaire est soumis 
-   if ( isset( $_POST['submit'] ) ) {
-     $recherche = $_POST['recherche']."."; 
-     //echo 'Recherche : '.$recherche; 
-   }
-   $j = 0;
-   for($i = 0 ; $i<strlen($recherche) ; $i++){// test si le nombre de doubles-quotes est impair
-	   if ($recherche[$i] == '"'){
-		   $j++;
-	   }
-   }
+	// Vérifier si le formulaire est soumis 
+	if ( isset( $_POST['submit'] ) ) {
+		$recherche = $_POST['recherche']."."; 
+		//echo 'Recherche : '.$recherche; 
+		$_SESSION['recherche'] = $recherche;
+	}
+	else {
+		$recherche = $_SESSION['recherche'];
+	}
+	$j = 0;
+	for($i = 0 ; $i<strlen($recherche) ; $i++){// test si le nombre de doubles-quotes est impair
+		if ($recherche[$i] == '"'){
+			$j++;
+		}
+	}
 	  
 	if ( $j%2 )
 		echo "Problème de syntaxe dans votre requête : nombre impair de double-quotes";
@@ -90,13 +94,91 @@
 			}
 			
 			$i++;
-		 }
-		 foreach($alimNonSouhaites as $alim ){
-			 
+		}
+		foreach($alimNonSouhaites as $alim ){	
 			echo "alimNonSouhaite: ".$alim."\n </br>";
-		 }
-		 foreach($alimSouhaites as $alim ){
+		}
+		foreach($alimSouhaites as $alim ){
 			echo"alimSouhaite: ".$alim. "\n </br>";
-		 }
+		}
+	}
+
+	// --------------------- CALCUL DU SCORE POUR CHAQUE RECETTE ---------------------
+	include("Donnees.inc.php");
+	// On sélectionne les recettes qui correspondent à $alimSouhaites et $alimNonSouhaites
+    $res = array();
+	$score = array(); // tableau des scores de chaque de recette initialisé à 100
+    foreach($Recettes as $numRecette => $Recette) {
+		$nbAlimSouhaitePresent = 0;
+		$nbAlimSouhaiteAbsent = 0;
+		$nbAlimNonSouhaiteAbsent = 0;
+		$nbAlimNonSouhaitePresent = 0;
+		foreach($Recette['index'] as $num => $ingredient) { // les ingredients PRESENT
+			$trouve = false;
+			$parcoursIngredient = $ingredient;
+			while($parcoursIngredient != "Aliment" && !$trouve) { // on cherche dans les super-categorie
+				if (in_array($parcoursIngredient, $alimSouhaites)) { // une categorie superieure de l'ingredient est dans les alimSouhaites
+					$nbAlimSouhaitePresent++;
+					$trouve = true;
+				}
+				if (in_array($parcoursIngredient, $alimNonSouhaites)) { // une categorie superieure de l'ingredient est dans les alimNonSouhaites
+					$nbAlimNonSouhaitePresent++;
+					$trouve = true;
+				}
+				if (in_array($parcoursIngredient, $alimSouhaites)) { // toutes les categories superieures de l'ingredient sont absentes de alimSouhaites
+					$nbAlimSouhaiteAbsent++;
+					$trouve = true;
+				}
+				if (in_array($parcoursIngredient, $alimNonSouhaites)) { // toutes les categories superieures de l'ingredient sont absentes de alimNonSouhaites
+					$nbAlimNonSouhaiteAbsent++;
+					$trouve = true;
+				}
+				$parcoursIngredient = $Hierarchie[$parcoursIngredient]["super-categorie"][0];
+			}
+			// Pour aliment
+			if (in_array($parcoursIngredient, $alimSouhaites)) { // une categorie superieure de l'ingredient est dans les alimSouhaites
+				$nbAlimSouhaitePresent++;
+			}
+			if (in_array($parcoursIngredient, $alimNonSouhaites)) { // une categorie superieure de l'ingredient est dans les alimNonSouhaites
+				$nbAlimNonSouhaitePresent++;
+			}
+			if (in_array($parcoursIngredient, $alimSouhaites)) { // toutes les categories superieures de l'ingredient sont absentes de alimSouhaites
+				$nbAlimSouhaiteAbsent++;
+			}
+			if (in_array($parcoursIngredient, $alimNonSouhaites)) { // toutes les categories superieures de l'ingredient sont absentes de alimNonSouhaites
+				$nbAlimNonSouhaiteAbsent++;
+			}
+		}
+
+		/* On a
+		$nbAlimSouhaitePresent
+		$nbAlimSouhaiteAbsent
+		$nbAlimNonSouhaiteAbsent
+		$nbAlimNonSouhaitePresent
+		pour calculer $score[$Recette]
+		*/
+
+		$nbCriteres = sizeof($alimSouhaites) + sizeof($alimNonSouhaites);
+		$score[$Recette['titre']] = 100*( ($nbAlimSouhaitePresent + $nbAlimNonSouhaiteAbsent)/$nbCriteres );
+
+		echo "Recette : ".$Recette['titre'].", nbAlimSouhaitePresent : ".$nbAlimSouhaitePresent.", nbAlimNonSouhaiteAbsent : ".$nbAlimNonSouhaiteAbsent."</br>";
+
+    }
+
+	include("traitementNomFichier.php"); // accès à la fonction traitementNomFichier($chaine)
+	arsort($score); // trie par ordre decroissant en gardant le lien index valeur
+	foreach($score as $titreRecette => $pourcent) {
+		if ($pourcent > 0) {
+			//echo $titreRecette;
+			$nomFichier = traitementNomFichier($titreRecette);
+			if (!file_exists("../Photos/".$nomFichier))
+				$nomFichier = "cocktail.png";
+			echo "
+				<div class=\"boisson border col-auto\">
+				<h5><a href=\"?recette=".$titreRecette."\">".$titreRecette."</a> <a><img class='heart' id=\"".$titreRecette."\" height=\"20\" width=\"20\" src=\"../Photos/coeur.png\"/></a></h5>
+				<img src=\"../Photos/".$nomFichier."\" alt=\"boisson\" height=\"100\">
+				<p>".$pourcent."</p>
+			";
+		}
 	}
 ?>
---
+++ Projet/src/traitementNomFichier.php
@@ -0,0 +1,13 @@
+<?php
+function traitementNomFichier($chaine) {
+    $string = str_replace(' ', '_', $chaine);
+    $search  = array('À', 'Á', 'Â', 'Ã', 'Ä', 'Å', 'Ç', 'È', 'É', 'Ê', 'Ë', 'Ì', 'Í', 'Î', 'Ï', 'Ò', 'Ó', 'Ô', 'Õ', 'Ö', 'Ù', 'Ú', 'Û', 'Ü', 'Ý', 'à', 'á', 'â', 'ã', 'ä', 'å', 'ç', 'è', 'é', 'ê', 'ë', 'ì', 'í', 'î', 'ï', 'ð', 'ò', 'ó', 'ô', 'õ', 'ö', 'ù', 'ú', 'û', 'ü', 'ý', 'ÿ', 'ñ');
+    $replace = array('A', 'A', 'A', 'A', 'A', 'A', 'C', 'E', 'E', 'E', 'E', 'I', 'I', 'I', 'I', 'O', 'O', 'O', 'O', 'O', 'U', 'U', 'U', 'U', 'Y', 'a', 'a', 'a', 'a', 'a', 'a', 'c', 'e', 'e', 'e', 'e', 'i', 'i', 'i', 'i', 'o', 'o', 'o', 'o', 'o', 'o', 'u', 'u', 'u', 'u', 'y', 'y', 'n');
+    $string = str_replace($search, $replace, $string);
+    $string = strtolower($string);
+    $string = ucfirst($string);
+    $string = preg_replace('/[^A-Za-z_]/', '', $string); // on enlève les caractères spéciaux
+    $string = $string.".jpg";
+    return $string;
+};
+?>
